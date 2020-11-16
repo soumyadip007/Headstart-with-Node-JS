3d753dc73b0b9c18b9e3b2941c19523c1cb9337a
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-16 21:09:07 +0530

    Module.js

--- Basic/module.js
+++ Basic/module.js
@@ -1,8 +1,8 @@
-function add(a,b){
-    return a+b
-}
+var calc = require('./calc.js')
 
-result=add(4,5)
+
+result=calc.add(4,5)
 
 
 console.log("The output is "+result);
+console.log("The output is "+calc.sub(10,100));
