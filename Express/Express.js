a4cdd7236324b8f2bd263fbc2e21e72d2aef84b7
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-16 21:50:42 +0530

    Add

--- Express/Express.js
+++ Express/Express.js
@@ -7,7 +7,29 @@ app.listen(9000, function(req,response){
 })
 
 app.get('/', function(req,res){
-
     console.log("Hello World")
-
+    res.send('Hello World')
 })
+
+
+app.get('/println', function(req,res){
+    res.send('Welcome to Println')
+})
+
+
+app.get('/data/:id', function(req,res){
+   const id=req.params.id
+   if(id==10)
+        res.send("Soumya")
+    else   
+        res.send("Dip")
+})
+
+
+app.get('/data', function(req,res){
+    const id=req.query.id
+    if(id==10)
+         res.send("Param-Soumya")
+     else   
+         res.send("Param-Dip")
+ })
