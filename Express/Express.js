64b7f5450bb610526ee39ea0ca29f783d79d9d96
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-16 21:34:36 +0530

    Written First API

--- Express/Express.js
+++ Express/Express.js
@@ -1,3 +1,13 @@
 const express= require('express')
 
 const app= express()
+
+app.listen(9000, function(req,response){
+    console.log('Running')
+})
+
+app.get('/', function(req,res){
+
+    console.log("Hello World")
+
+})
