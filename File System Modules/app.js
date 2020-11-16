83dd79e72af2ea5b7b96526e8a4bde42943fa105
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-16 21:18:48 +0530

    Module

--- File System Modules/app.js
+++ File System Modules/app.js
@@ -6,3 +6,12 @@ fs.readFile('calc.js','utf8',function(err,data){
     console.log(data)
 
 })
+
+fs.writeFile('calc1.js','console.log("Done")',function(err){
+    console.log(err)
+})
+
+
+// fs.unlink('calc1.js',function(err){
+//     console.log(err)
+// })
